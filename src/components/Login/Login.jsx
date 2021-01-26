94bdc457aea82179bc30fc495479eb399053f5f4
Felix Pohlmann <felixpohlmann.dev@gmail.com>
2021-01-26 20:14:12 +0100

    basic form setup

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 
-import { Container, Row, Col, Form } from "react-bootstrap/";
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  InputGroup,
+  Button,
+} from "react-bootstrap/";
+import { AiOutlineUser, AiOutlineKey } from "react-icons/ai";
 import authenticationIllustration from "./authentication.svg";
 
 import "./Login.css";
@@ -10,9 +18,38 @@ const Login = () => {
     <Container fluid>
       <Row className="row__main">
         <Col className="wrapper__form" xs={6}>
-          Login-Form
+          <Container>
+            <Row>
+              <Col className="form__header">
+                <h1>Login</h1>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <InputGroup className="form__input">
+                  <InputGroup.Prepend>
+                    <InputGroup.Text>
+                      <AiOutlineUser size="1.2rem" />
+                    </InputGroup.Text>
+                  </InputGroup.Prepend>
+                  <Form.Control placeholder="username" aria-label="username" />
+                </InputGroup>
+
+                <InputGroup className="form__input">
+                  <InputGroup.Prepend>
+                    <InputGroup.Text>
+                      <AiOutlineKey size="1.2rem" />
+                    </InputGroup.Text>
+                  </InputGroup.Prepend>
+                  <Form.Control placeholder="password" aria-label="password" />
+                </InputGroup>
+
+                <Button className="form__button">Sign In</Button>
+              </Col>
+            </Row>
+          </Container>
         </Col>
-        <Col className="wrapper__illustration" xs={6} >
+        <Col className="wrapper__illustration" xs={6}>
           <img
             className="illustration"
             src={authenticationIllustration}
