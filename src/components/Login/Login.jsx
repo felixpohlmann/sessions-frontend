f44a78845e6638841ad1c1dad4b5d73287cd0e2a
Felix Pohlmann <felixpohlmann.dev@gmail.com>
2021-01-26 17:19:22 +0100

    added illustration

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-import { Container, Row, Col, Form, Button } from "react-bootstrap/";
+import { Container, Row, Col, Form } from "react-bootstrap/";
+import authenticationIllustration from "./authentication.svg";
 
 import "./Login.css";
 
@@ -8,8 +9,16 @@ const Login = () => {
   return (
     <Container fluid>
       <Row className="row__main">
-        <Col className="wrapper__form">Login-Form</Col>
-        <Col className="wrapper__illustration">Illustration</Col>
+        <Col className="wrapper__form" xs={6}>
+          Login-Form
+        </Col>
+        <Col className="wrapper__illustration" xs={6} >
+          <img
+            className="illustration"
+            src={authenticationIllustration}
+            alt="Authenticate"
+          />
+        </Col>
       </Row>
     </Container>
   );
