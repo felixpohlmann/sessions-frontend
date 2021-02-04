284fdcecee57e24bcf5eb4ff44983681695824bb
Felix Pohlmann <felixpohlmann.dev@gmail.com>
2021-02-04 21:47:45 +0100

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
+import React, { Component } from "react";
+
 import Login from "./components/Login/Login";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./global/colors.css";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="app">
-      <Login />
-    </div>
-  );
+class App extends Component {
+  state = { token: "" };
+
+  handleTokenSet = (token) => {
+    this.setState({ token });
+  };
+
+  render() {
+    const { handleTokenSet } = this;
+    return (
+      <div className="app">
+        <Login onTokenSet={handleTokenSet} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
 import {
   Container,
@@ -13,52 +14,91 @@ import authenticationIllustration from "./authentication.svg";
 
 import "./Login.css";
 
-const Login = () => {
-  return (
-    <Container fluid>
-      <Row className="row__main">
-        <Col className="wrapper__form" xs={6}>
-          <Container>
-            <Row>
-              <Col className="form__header">
-                <h1>Login</h1>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <InputGroup className="form__input">
-                  <InputGroup.Prepend>
-                    <InputGroup.Text>
-                      <AiOutlineUser size="1.2rem" />
-                    </InputGroup.Text>
-                  </InputGroup.Prepend>
-                  <Form.Control placeholder="username" aria-label="username" />
-                </InputGroup>
+class Login extends Component {
+  state = {
+    inputUsername: null,
+    inputPassword: null,
+  };
 
-                <InputGroup className="form__input">
-                  <InputGroup.Prepend>
-                    <InputGroup.Text>
-                      <AiOutlineKey size="1.2rem" />
-                    </InputGroup.Text>
-                  </InputGroup.Prepend>
-                  <Form.Control placeholder="password" aria-label="password" />
-                </InputGroup>
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
 
-                <Button className="form__button">Sign In</Button>
-              </Col>
-            </Row>
-          </Container>
-        </Col>
-        <Col className="wrapper__illustration" xs={6}>
-          <img
-            className="illustration"
-            src={authenticationIllustration}
-            alt="Authenticate"
-          />
-        </Col>
-      </Row>
-    </Container>
-  );
-};
+  handleSubmit = async () => {
+    const { inputPassword, inputUsername } = this.state;
+    const { onTokenSet } = this.props;
+    const response = await axios.post("http://localhost:5000/api/auth/signin", {
+      username: inputUsername,
+      password: inputPassword,
+    });
+    if (response.status === 200) {
+      onTokenSet(response.data.token);
+    }
+  };
+
+  render() {
+    const { handleChange, handleSubmit } = this;
+
+    return (
+      <Container fluid>
+        <Row className="row__main">
+          <Col className="wrapper__form" xs={6}>
+            <Container>
+              <Row>
+                <Col className="form__header">
+                  <h1>Login</h1>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <InputGroup className="form__input">
+                    <InputGroup.Prepend>
+                      <InputGroup.Text>
+                        <AiOutlineUser />
+                      </InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <Form.Control
+                      placeholder="Username"
+                      aria-label="Username"
+                      onChange={(e) => handleChange(e)}
+                      name="inputUsername"
+                    />
+                  </InputGroup>
+
+                  <InputGroup className="form__input">
+                    <InputGroup.Prepend>
+                      <InputGroup.Text>
+                        <AiOutlineKey />
+                      </InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <Form.Control
+                      type="password"
+                      placeholder="Password"
+                      aria-label="Password"
+                      onChange={(e) => handleChange(e)}
+                      name="inputPassword"
+                    />
+                  </InputGroup>
+
+                  <Button className="form__button" onClick={handleSubmit}>
+                    Sign In
+                  </Button>
+                </Col>
+              </Row>
+            </Container>
+          </Col>
+          <Col className="wrapper__illustration" xs={6}>
+            <img
+              className="illustration"
+              src={authenticationIllustration}
+              alt="Authenticate"
+            />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
 
 export default Login;
